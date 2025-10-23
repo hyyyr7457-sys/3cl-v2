ef41398186538bd8177ae06eed4b19adf2a70375
hyyyr7457-sys <hyyyr7457@gmail.com>
2025-10-23 10:30:11 -0500

    Add dialogues.js with initial dialogue data

---
+++ dialogue.js
@@ -0,0 +1,12 @@
+const dialogues = [
+  {
+    speaker: "Teacher",
+    text: "Hello, class! Today we will learn about greetings.",
+    audioFile: "audios/teacher-greetings.mp3"
+  },
+  {
+    speaker: "Student",
+    text: "Hi, teacher! I'm ready to learn.",
+    audioFile: "audios/student-ready.mp3"
+  }
+];
